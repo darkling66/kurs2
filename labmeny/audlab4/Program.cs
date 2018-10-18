3d7ff2cf46f807c77a7bf8f82302fe5c2f15e601
darkling6667 <35731422+darkling66@users.noreply.github.com>
2018-10-18 11:14:06 +0300

    Update Program.cs

--- labmeny/audlab4/Program.cs
+++ labmeny/audlab4/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,47 +8,36 @@ namespace Lab_4
 {
     class Program
     {
-        const double StartX = 10.3;
+        const double StartX = 0.1;
+        const double StartY = .2;
         const double dX = 0.7;
+        const double dY = 0.6;
         static double Function(double x)
         {
             return 23 * Math.Pow(Math.Sin(Math.Pow(2.76 * x, 3)) * Math.Pow(0.5 * x, 5), 2) + 2 * (2.76 * x) + Math.Cos((2.76 * x) * (0.5 * x));
         }
+        static double Function2(double x1, double x2)
+        {
+            return 23 * Math.Pow(Math.Sin(Math.Pow( x1, 3)) * Math.Pow(x2, 5), 2) + 2 * x1 + Math.Cos( x1 * x2);
+        }
         static void Main(string[] args)
         {
             double x = StartX;
-            double[,] arr = new double[5, 5];
+            double y = StartY;
+            double[,] arr = new double[3, 3];
             for (int i = arr.GetLowerBound(0); i <= arr.GetUpperBound(0); i++)
             {
                 for (int j = arr.GetLowerBound(1); j <= arr.GetUpperBound(1); j++)
                 {
-                    arr[i, j] = Function(x);
+                    arr[i, j] = Function2(x,y);
                     x += dX;
-                }
-            }
-            Console.WriteLine("Вiдсортованi за спаданням значення масиву: ");
-            Console.WriteLine();
-            for (int i = arr.GetLowerBound(0); i <= arr.GetUpperBound(0); i++)
-            {
-                for (int j = arr.GetLowerBound(1); j <= arr.GetUpperBound(1); j++)
-                {
-                    for (int q = i; q <= arr.GetUpperBound(0); q++)
-                    {
-                        for (int w = (q == i) ? j : 0; w <= arr.GetUpperBound(1); w++)
-                        {
-                            if (arr[i, j] < arr[q, w])
-                            {
-                                double temp = arr[q, w];
-                                arr[q, w] = arr[i, j];
-                                arr[i, j] = temp;
-                            }
-                        }
-                    }
+                    y += dY;
                     arr[i, j] = Math.Round(arr[i, j], 4);
                     Console.Write(" " + arr[i, j]);
                 }
                 Console.WriteLine();
             }
+            
             Console.WriteLine();
             double aMin = 0;
             double aMax = 0;
@@ -58,8 +47,9 @@ namespace Lab_4
             {
                 for (int j = arr.GetLowerBound(1); j <= arr.GetUpperBound(1); j++)
                 {
-                    aMin = arr[arr.GetUpperBound(0), arr.GetUpperBound(1)];
-                    aMax = arr[arr.GetLowerBound(0), arr.GetLowerBound(1)];
+                    aMin = arr[arr.GetLowerBound(0), arr.GetLowerBound(1)];
+                    aMax = arr[arr.GetUpperBound(0), arr.GetUpperBound(1)];
+                    
                     aAvg += arr[i, j];
                 }
             }
