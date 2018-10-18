06d7834aa5593974c142d831350e6d635317793e
darkling6667 <35731422+darkling66@users.noreply.github.com>
2018-10-18 10:41:36 +0300

    Add files via upload

---
+++ labmeny/audlab4/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    class Program
+    {
+        const double StartX = 10.3;
+        const double dX = 0.7;
+        static double Function(double x)
+        {
+            return 23 * Math.Pow(Math.Sin(Math.Pow(2.76 * x, 3)) * Math.Pow(0.5 * x, 5), 2) + 2 * (2.76 * x) + Math.Cos((2.76 * x) * (0.5 * x));
+        }
+        static void Main(string[] args)
+        {
+            double x = StartX;
+            double[,] arr = new double[5, 5];
+            for (int i = arr.GetLowerBound(0); i <= arr.GetUpperBound(0); i++)
+            {
+                for (int j = arr.GetLowerBound(1); j <= arr.GetUpperBound(1); j++)
+                {
+                    arr[i, j] = Function(x);
+                    x += dX;
+                }
+            }
+            Console.WriteLine("Вiдсортованi за спаданням значення масиву: ");
+            Console.WriteLine();
+            for (int i = arr.GetLowerBound(0); i <= arr.GetUpperBound(0); i++)
+            {
+                for (int j = arr.GetLowerBound(1); j <= arr.GetUpperBound(1); j++)
+                {
+                    for (int q = i; q <= arr.GetUpperBound(0); q++)
+                    {
+                        for (int w = (q == i) ? j : 0; w <= arr.GetUpperBound(1); w++)
+                        {
+                            if (arr[i, j] < arr[q, w])
+                            {
+                                double temp = arr[q, w];
+                                arr[q, w] = arr[i, j];
+                                arr[i, j] = temp;
+                            }
+                        }
+                    }
+                    arr[i, j] = Math.Round(arr[i, j], 4);
+                    Console.Write(" " + arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            double aMin = 0;
+            double aMax = 0;
+            double aAvg = 0;
+            double sum = 0;
+            for (int i = arr.GetLowerBound(0); i <= arr.GetUpperBound(0); i++)
+            {
+                for (int j = arr.GetLowerBound(1); j <= arr.GetUpperBound(1); j++)
+                {
+                    aMin = arr[arr.GetUpperBound(0), arr.GetUpperBound(1)];
+                    aMax = arr[arr.GetLowerBound(0), arr.GetLowerBound(1)];
+                    aAvg += arr[i, j];
+                }
+            }
+            aAvg = aAvg / arr.Length;
+            for (int i = arr.GetLowerBound(0); i <= arr.GetUpperBound(0); i++)
+            {
+                for (int j = arr.GetLowerBound(1); j <= arr.GetUpperBound(1); j++)
+                {
+                    if (arr[i, j] >= aMin && arr[i, j] <= aMin + 0.1 * aMin)
+                    {
+                        sum += arr[i, j];
+                    }
+                }
+            }
+            Console.WriteLine("Мiнiмальне значення масиву: {0:0.0000}", aMin);
+            Console.WriteLine("Максимальне значення масиву: {0:0.0000}", aMax);
+            Console.WriteLine("Середнє значення масиву: {0:0.0000}", aAvg);
+            Console.WriteLine("Сума елементiв в дiапазонi: {0:0.0000}", sum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ labmeny/labmeny/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3_1
+{
+    public delegate double FuncDelegate(double x);
+    class Program
+    {
+        static int Menu()
+        {
+            Console.WriteLine("Обчислення інтегралу");
+            Console.WriteLine("0. Метод лівих прямокутників");
+            Console.WriteLine("1. Метод правих прямокутників");
+            Console.WriteLine("2. Метод центральних прямокутників");
+            var res = Console.ReadLine();
+            return Convert.ToInt32(res);
+        }
+
+        public static double QuadCentral(double a, double b, double n, FuncDelegate func)
+        {
+            Console.WriteLine("Метод центральних прямокутників");
+            double h = (b - a) / n;
+            double q = 0;
+            for (double x = a; x < b; x += h)
+            {
+                q += func(x + h / 2);
+            }
+            return q * h;
+        }
+
+        public static double QuadLeft(double a, double b, double n, FuncDelegate func)
+        {
+            Console.WriteLine("---------------------Метод лівих прямокутників------------------");
+            double h = (b - a) / n;
+            double y1;
+            double x1;
+            double Intgrl = 0;
+            for (int i = 0; i < n; i++)
+            {
+                x1 = a + i * h;
+                y1 = func(x1);
+                Intgrl += y1 * h;
+            }
+            return Intgrl;
+        }
+
+        public static double QuadRight(double a, double b, double n, FuncDelegate func)
+        {
+            Console.WriteLine("---------------------Метод правих прямокутників------------------");
+            double h = (b - a) / n;
+            double y1;
+            double x1;
+            double Intgrl = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                x1 = a + i * h;
+                y1 = func(x1);
+                Intgrl += y1 * h;
+            }
+            return Intgrl;
+        }
+
+        delegate double QuadDelegate(double a, double b, double n, FuncDelegate func);
+
+        static double F(double x)
+        {
+            return (Math.Pow(Math.Sin(x * 2), 3) + 45 + (5 * x)) / (Math.Pow((2 * (2 * x)), 4) + (4 * x));
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введiть початок вiдрiзку iнтегрування a: ");
+            string sa = Console.ReadLine();
+            double a = double.Parse(sa);
+            Console.Write("Введiть кiнець вiдрiзку iнтегрування b: ");
+            string sb = Console.ReadLine();
+            double b = double.Parse(sb);
+            Console.Write("Введiть кiлькiсть дiлянок n: ");
+            string sn = Console.ReadLine();
+            double n = double.Parse(sn);
+            QuadDelegate[] menu = { QuadLeft, QuadRight, QuadCentral };
+            while (true)
+            {
+                QuadDelegate q = menu[Menu()];
+                double r = q(a, b, n, F);
+                Console.WriteLine("Integral: [{0}, {1}] = {2}", a, b, r);
+                Console.WriteLine();
+            }
+        }
+    }
+}
