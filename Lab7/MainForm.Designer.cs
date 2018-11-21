bae884993d5da97b44de9b3ab47f030d8742dcb6
darkling6667 <35731422+darkling66@users.noreply.github.com>
2018-11-22 00:46:14 +0200

    Add files via upload

---
+++ Lab7/MainForm.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Lab_7
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbY = new System.Windows.Forms.TextBox();
+            this.tbX2 = new System.Windows.Forms.TextBox();
+            this.tbX1 = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.tbMin = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Змінна Х1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Змінна Х1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Результат розрахунку Y";
+            // 
+            // tbY
+            // 
+            this.tbY.Location = new System.Drawing.Point(172, 160);
+            this.tbY.Name = "tbY";
+            this.tbY.ReadOnly = true;
+            this.tbY.Size = new System.Drawing.Size(100, 20);
+            this.tbY.TabIndex = 3;
+            // 
+            // tbX2
+            // 
+            this.tbX2.Location = new System.Drawing.Point(172, 105);
+            this.tbX2.Name = "tbX2";
+            this.tbX2.Size = new System.Drawing.Size(100, 20);
+            this.tbX2.TabIndex = 4;
+            // 
+            // tbX1
+            // 
+            this.tbX1.Location = new System.Drawing.Point(172, 49);
+            this.tbX1.Name = "tbX1";
+            this.tbX1.Size = new System.Drawing.Size(100, 20);
+            this.tbX1.TabIndex = 5;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(15, 268);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Обчислити";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(197, 268);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Вихід";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // tbMin
+            // 
+            this.tbMin.Location = new System.Drawing.Point(172, 216);
+            this.tbMin.Name = "tbMin";
+            this.tbMin.ReadOnly = true;
+            this.tbMin.Size = new System.Drawing.Size(100, 20);
+            this.tbMin.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Менше із значень Х1,Х2";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(106, 268);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Очистити";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 315);
+            this.Controls.Add(this.tbMin);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.tbX1);
+            this.Controls.Add(this.tbX2);
+            this.Controls.Add(this.tbY);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Лабораторна робота №7";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbY;
+        private System.Windows.Forms.TextBox tbX2;
+        private System.Windows.Forms.TextBox tbX1;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox tbMin;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Lab7/MainForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_7
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbX1.Text) || (String.IsNullOrEmpty(tbX2.Text)))
+            {
+                tbY.Text = "Не введено даних!";
+                tbMin.Text = "Не введено даних!";
+                return;
+            }
+            double x1 = double.Parse(tbX1.Text);
+            double x2 = double.Parse(tbX2.Text);
+            double y = Math.Pow(Math.Cos(Math.Exp((x1 + 2 * x2 + 9) / 0.666)), 3);
+            tbY.Text = y.ToString("0.######");
+            if (x1 < x2) {
+                tbMin.Text = x1.ToString("0.######");
+            }
+            else
+            {
+                tbMin.Text = x2.ToString("0.######");
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tbX1.Text = string.Empty;
+            tbX2.Text = string.Empty;
+            tbY.Text = string.Empty;
+            tbMin.Text = string.Empty;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
