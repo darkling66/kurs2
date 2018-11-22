5beb5e9b201feb9b9803c75e5bd1b5477fc2d21f
darkling6667 <35731422+darkling66@users.noreply.github.com>
2018-11-22 23:21:40 +0200

    Update MainForm.cs

--- Lab7/MainForm.cs
+++ Lab7/MainForm.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -24,14 +24,24 @@ namespace Lab_7
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
+            double x1 = 0;
+            double x2 = 0;
             if (string.IsNullOrEmpty(tbX1.Text) || (String.IsNullOrEmpty(tbX2.Text)))
             {
                 tbY.Text = "Не введено даних!";
                 tbMin.Text = "Не введено даних!";
                 return;
             }
-            double x1 = double.Parse(tbX1.Text);
-            double x2 = double.Parse(tbX2.Text);
+            try
+            {
+                x1 = double.Parse(tbX1.Text);
+                x2 = double.Parse(tbX2.Text);
+            }
+            catch(Exception excpetion)
+            {
+                string error = String.Format("Error number. {0}", excpetion.Message);
+                MessageBox.Show(error, "Conversion error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
             double y = Math.Pow(Math.Cos(Math.Exp((x1 + 2 * x2 + 9) / 0.666)), 3);
             tbY.Text = y.ToString("0.######");
             if (x1 < x2) {
