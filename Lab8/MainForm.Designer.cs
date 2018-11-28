10642d876984e2eae04e4c26ec6124b09fe93bca
darkling6667 <35731422+darkling66@users.noreply.github.com>
2018-11-28 20:53:09 +0200

    Add files via upload

---
+++ Lab8/MainForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Lab8
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbLakeInfo = new System.Windows.Forms.TextBox();
+            this.btnAddLake = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbLakeInfo
+            // 
+            this.tbLakeInfo.Location = new System.Drawing.Point(11, 10);
+            this.tbLakeInfo.Multiline = true;
+            this.tbLakeInfo.Name = "tbLakeInfo";
+            this.tbLakeInfo.ReadOnly = true;
+            this.tbLakeInfo.Size = new System.Drawing.Size(459, 239);
+            this.tbLakeInfo.TabIndex = 0;
+            // 
+            // btnAddLake
+            // 
+            this.btnAddLake.Location = new System.Drawing.Point(478, 10);
+            this.btnAddLake.Name = "btnAddLake";
+            this.btnAddLake.Size = new System.Drawing.Size(77, 35);
+            this.btnAddLake.TabIndex = 1;
+            this.btnAddLake.Text = "Добавить озеро";
+            this.btnAddLake.UseVisualStyleBackColor = true;
+            this.btnAddLake.Click += new System.EventHandler(this.btnAddLake_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(478, 223);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(77, 26);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Закрыть";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 261);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnAddLake);
+            this.Controls.Add(this.tbLakeInfo);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Лабораторна робота №8";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbLakeInfo;
+        private System.Windows.Forms.Button btnAddLake;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ Lab8/MainForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab8
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddLake_Click(object sender, EventArgs e)
+        {
+            Lake lake = new Lake();
+            LakeForm lf = new LakeForm(lake);
+            if(lf.ShowDialog() == DialogResult.OK)
+            {
+                tbLakeInfo.Text += string.Format(
+                "Данные про Озеро:\r\n" +
+                "Вид: {0}\r\n" +
+                "Глубина: {1} м\r\n" +
+                "Ширина: {2} км\r\n" +
+                "Имя: {3}\r\n" +
+                "Возраст: {4} млн\r\n" +
+                "Загрязненность: {5}%\r\n" +
+                " \r\n", lake.Type, lake.Depth, lake.Weidth, lake.Name, lake.Age, lake.Pollution);
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Прекратить роботу?", "Завершение работы", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
